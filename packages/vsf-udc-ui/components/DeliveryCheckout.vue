b35722d586b7dbbd1fa6caa590ca60daacd3b922
robin-nilsson <robin@robins.nu>
2019-07-25 15:12:39 +0200

    ED-457 Integrate UDC with VSF's shipping

--- packages/vsf-udc-ui/components/DeliveryCheckout.vue
+++ packages/vsf-udc-ui/components/DeliveryCheckout.vue
@@ -1,73 +1,47 @@
 <template>
   <div>
-    <div class="row">
-      <div class="col-xs-11 col-sm-9 col-md-11">
-        <div class="row mb15">
-          <div class="col-xs-12 col-md-7">
-            <h3 class="m0 mb5 shipping-title weight-700">
-              2. {{ $t('Shipping') }}
-            </h3>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="row m0">
-      <div class="col-md-12">
-        <div class="row bg-white">
-          <div class="col-md-12 p0">
-            <label class="radioStyled p20">
-              <div class="row shipping-methods weight-700">
-                <div class="col-md-2 col-sm-3 col-xs-4 px15 align-center">
-                  <!-- <img :src="`/assets/logos/${method.method_code}.png`" alt=""> -->
-                </div>
-                <div class="col-md-8 col-sm-8 col-xs-8">
-                  <p class="weight-400 m0 small-text">method.carrier_title </p>
-                  <span>method.method_title<span class="hidden-md">(method.amount | price )</span></span>
-                </div>
-                <span class="hidden-xs">method.amount | price</span>
-              </div>
-              <input type="radio">
-              <span class="checkmark" />
-              <span class="checkbox-border" />
-            </label>
-          </div>
-          <span class="validation-error">
-            {{ $t('Field is required') }}
-          </span>
-        </div>
-      </div>
-    </div>
-    <div ref="udc" />
+    <div ref="udc" class="udc-widget" />
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import { required } from 'vuelidate/lib/validators'
 import { loadScript } from '../helpers'
 import { isServer } from '@vue-storefront/core/helpers'
+import { currentStoreView } from '@vue-storefront/core/lib/multistore'
 
 export default {
   async mounted () {
     if (!isServer) {
       await this.renderUdc()
-      this.mapShippingMethods()
+      this.mapUnifaunShippingMethods()
     }
+  },
+  computed: {
+    ...mapGetters({
+      shippingMethods: 'shipping/shippingMethods'
+    })
   },
   data () {
     return {
-      shippingMethods: null,
       widget: null
+    }
+  },
+  validations: {
+    shipping: {
+      shippingMethod: {
+        required
+      }
     }
   },
   methods: {
     async renderUdc () {
+      const defaultLanguage = currentStoreView().i18n.defaultLanguage
       await loadScript('https://api.unifaun.com/rs-extapi/v1/delivery-checkouts-widget/unifaun-checkout-all.min.js', 'udc')
       this.widget = window.UnifaunCheckout.createAt(this.$refs.udc, {
-        language: 'se',
-        disabled: false,
-        useIcons: true,
-        resultCallback: function (cb) {
-          console.log('udc callback: ', cb)
-        }
+        language: defaultLanguage,
+        disabled: false
       })
 
       const username = 'WPWHV5VISQK7NZTN'
@@ -82,29 +56,48 @@ export default {
       const json = await response.json()
       json.jsonPayload = JSON.parse(json.jsonPayload)
       this.widget.updateList(json)
-      console.log('udc widget', this.widget)
     },
-    mapShippingMethods () {
-      this.shippingMethods = this.widget.model.options.map(option => {
-        let subOptions = []
-        console.log('udc option')
+    mapUnifaunShippingMethods () {
+      this.widget.model.options.forEach(option => {
         if (option.subOptions) {
-          console.log('udc suboptions')
-          subOptions = option.subOptions.map(suboption => ({
-            name: suboption.name,
-            description: suboption.description1,
-            carrierId: suboption.carrierId | ''
-          }))
-        }
-
-        return {
-          name: option.name,
-          description: option.description1,
-          subOptions,
-          carrierId: option.carrierId
+          option.subOptions.forEach(subOption => {
+            this.shippingMethods.push({
+              amount: subOption.priceValue || 0,
+              available: true,
+              base_amount: subOption.priceValue || 0,
+              carrier_code: subOption.carrierId || '',
+              carrier_title: subOption.description1,
+              error_message: '',
+              method_code: subOption.carrierId || '',
+              method_title: subOption.name,
+              price_excl_tax: subOption.priceValue || 0,
+              price_incl_tax: subOption.priceValue || 0
+            })
+          })
+        } else {
+          this.shippingMethods.push({
+            amount: option.priceValue || 0,
+            available: true,
+            base_amount: option.priceValue || 0,
+            carrier_code: option.carrierId || '',
+            carrier_title: option.description1,
+            error_message: '',
+            method_code: option.carrierId || '',
+            method_title: option.name,
+            price_excl_tax: option.priceValue || 0,
+            price_incl_tax: option.priceValue || 0
+          })
         }
       })
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '~theme/css/variables/variables';
+
+.udc-widget {
+  display: none;
+}
+</style>
