d713d18b8b045e04a3134e665b64ced27b46fd86
robin-nilsson <robin@robins.nu>
2019-07-25 18:02:05 +0200

    Move auth and unifaun endpoint to config

--- packages/vsf-udc-ui/store/actions.ts
+++ packages/vsf-udc-ui/store/actions.ts
@@ -1,18 +1,19 @@
 import { UnifaunState } from '../types/UnifaunState'
 import { ActionTree } from 'vuex'
 import RootState from '@vue-storefront/core/types/RootState'
+import config from 'config'
 
 export const actions: ActionTree<UnifaunState, RootState> = {
   async loadUdc ({ commit, getters, dispatch }, { ref, client }) {
     const widget = client.createAt(ref, getters.unifaunOptions)
-    const username = 'WPWHV5VISQK7NZTN'
-    const password = 'VIGI53KEQHCN7FRGBF2HDDA7'
+    const username = config.unifaun.username
+    const password = config.unifaun.password
     const headers = new Headers()
     headers.set('Authorization', 'Basic ' + Buffer.from(username + ':' + password).toString('base64'))
     headers.set('Content-Type', 'application/json')
-    const response = await fetch('https://api.unifaun.com/rs-extapi/v1/delivery-checkouts/96d9eaad-018b-4dba-b4b8-7842fcd82440?language=sv&currency=sek&tocountry=se&tozipcode=34371', {
+    const response = await fetch(config.unifaun.endpoint, {
       method: 'GET',
-      headers: headers
+      headers
     })
     const json = await response.json()
     widget.updateList(json)
