ca651fb91caea8a58f4fa5820b03ed88d3ab4908
Robin Nilsson <robin@robins.nu>
2019-08-23 06:56:19 +0200

    Set method and carrier codes to values from config (#2)

--- packages/vsf-udc-ui/store/getters.ts
+++ packages/vsf-udc-ui/store/getters.ts
@@ -2,21 +2,22 @@ import { GetterTree } from 'vuex'
 import { UnifaunState } from '../types/UnifaunState'
 import RootState from '@vue-storefront/core/types/RootState'
 import { currentStoreView } from '@vue-storefront/core/lib/multistore'
+import config from 'config'
 
 export const getters: GetterTree<UnifaunState, RootState> = {
   shippingMethods (state: UnifaunState) {
     const shippingMethods = []
     state.options.forEach(option => {
-      if (option.subOptions.length > 0) {
+      if (option.subOptions && option.subOptions.length > 0) {
         option.subOptions.forEach(subOption => {
           shippingMethods.push({
             amount: subOption.priceValue || 0,
             available: true,
             base_amount: subOption.priceValue || 0,
-            carrier_code: subOption.carrierId || '',
+            carrier_code: config.unifaun.carrier_codes[subOption.serviceId.toLowerCase()] || '',
             carrier_title: subOption.description1,
             error_message: '',
-            method_code: subOption.carrierId || '',
+            method_code: config.unifaun.carrier_codes[subOption.serviceId.toLowerCase()] || '',
             method_title: subOption.name,
             price_excl_tax: subOption.priceValue || 0,
             price_incl_tax: subOption.priceValue || 0
@@ -27,10 +28,10 @@ export const getters: GetterTree<UnifaunState, RootState> = {
           amount: option.priceValue || 0,
           available: true,
           base_amount: option.priceValue || 0,
-          carrier_code: option.carrierId || '',
+          carrier_code: config.unifaun.carrier_codes[option.serviceId.toLowerCase()] || '',
           carrier_title: option.description1,
           error_message: '',
-          method_code: option.carrierId || '',
+          method_code: config.unifaun.carrier_codes[option.serviceId.toLowerCase()] || '',
           method_title: option.name,
           price_excl_tax: option.priceValue || 0,
           price_incl_tax: option.priceValue || 0
