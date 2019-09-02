2d6fb8a9f0b58ed3c4cd5b0fec3a9bcd0ad2124c
robin-nilsson <robin@robins.nu>
2019-09-02 12:53:58 +0200

    Remove validation for postnord for now

--- packages/vsf-udc-ui/components/DeliveryCheckout.vue
+++ packages/vsf-udc-ui/components/DeliveryCheckout.vue
@@ -11,7 +11,7 @@ import { isServer } from '@vue-storefront/core/helpers'
 export default {
   data () {
     return {
-      currentData: ''
+      currentData: {}
     }
   },
   beforeMount () {
@@ -20,7 +20,6 @@ export default {
         if (JSON.stringify(data) !== this.currentData) {
           this.currentData = JSON.stringify(data)
           await this.$store.dispatch('unifaun-delivery-checkout/setShippingAddress', { data })
-          await this.$store.dispatch('unifaun-delivery-checkout/validateForPostNord')
           await loadScript('https://api.unifaun.com/rs-extapi/v1/delivery-checkouts-widget/unifaun-checkout-all.min.js', 'udc')
           await this.$store.dispatch('unifaun-delivery-checkout/loadUdc', { ref: this.$refs.udc, client: window.UnifaunCheckout })
         }
